2f1de63683ca5d5410d52c8c788f30d0522e9cbc
Syed Muhammad Mujtaba Naqvi <smmujtabanaqvi315@gmail.com>
2024-07-18 01:23:42 -0700

    commit

---
+++ src/Cup.jsx
@@ -0,0 +1,11 @@
+import './cup.css';
+
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2 className="cup-h2">Tea cup for guest #{guest}</h2>;
+}
+
+export default Cup;
